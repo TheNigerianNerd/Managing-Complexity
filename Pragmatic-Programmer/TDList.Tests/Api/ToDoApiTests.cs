7141c36bf096ab5ced13af69c2c4374e9d71ad12
TheNigerianNerd <142974826+TheNigerianNerd@users.noreply.github.com>
2025-09-11 17:10:48 -0400

    Include test for 'weatherforecast' endpoint

--- Pragmatic-Programmer/TDList.Tests/Api/ToDoApiTests.cs
+++ Pragmatic-Programmer/TDList.Tests/Api/ToDoApiTests.cs
@@ -2,27 +2,28 @@ using TDList.Data;
 using TDList.Contracts;
 
 namespace TDList.Tests.Api;
-public class ToDoApiTests 
+
+public class ToDoApiTests
 {
     private readonly IDataSource _dataSource;
     private string _expectedFileName;
-    
+
     public ToDoApiTests()
     {
         _dataSource = new DataSource();
         _expectedFileName = DataSource.FileName;
-    }   
+    }
     [Fact]
-        public async Task GetWeatherForecast_ReturnsOkResult()
-        {
-            // Arrange
-            var client = new HttpClient();
-            var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:5001/weatherforecast");
+    public async Task GetWeatherForecast_ReturnsOkResult()
+    {
+        // Arrange
+        var client = new HttpClient();
+        var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7101/weatherforecast");
 
-            // Act
-            var response = await client.SendAsync(request);
+        // Act
+        var response = await client.SendAsync(request);
 
-            // Assert
-            Assert.True(response.IsSuccessStatusCode);
-        }
+        // Assert
+        Assert.True(response.IsSuccessStatusCode);
+    }
 }
---
+++ Pragmatic-Programmer/ToDoApi/Controllers/ToDoController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TDList.Data;
+
+namespace TDList.Tests
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ToDoController : ControllerBase
+    {
+        private readonly DataSource _dataSource;
+
+        public ToDoController(DataSource dataSource)
+        {
+            _dataSource = dataSource;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var toDos = _dataSource.Read();
+            return Ok(toDos);
+        }
+    }
+}
