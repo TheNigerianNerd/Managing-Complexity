8f67b57e417619f594e51200bce854d556596d93
TheNigerianNerd <142974826+TheNigerianNerd@users.noreply.github.com>
2025-09-17 20:16:14 -0400

    Eliminate unnecessary 'WeatherForecast' test
        # Co-authored-by: Oche "The Nigerian Nerd" Edache

--- Pragmatic-Programmer/TDList.Tests/Api/ToDoApiTests.cs
+++ Pragmatic-Programmer/TDList.Tests/Api/ToDoApiTests.cs
@@ -16,17 +16,4 @@ public class ToDoApiTests
         _expectedFileName = DataSource.FileName;
         _expectedTodos = _dataSource.Read();
     }
-    // [Fact]
-    // public async Task GetWeatherForecast_ReturnsOkResult()
-    // {
-    //     // Arrange
-    //     var client = new HttpClient();
-    //     var request = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7101/weatherforecast");
-
-    //     // Act
-    //     var response = await client.SendAsync(request);
-
-    //     // Assert
-    //     Assert.True(response.IsSuccessStatusCode);
-    // }
 }
