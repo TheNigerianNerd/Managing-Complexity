acb524fc5146d89eb4db0ba430773d546850d11f
TheNigerianNerd <142974826+TheNigerianNerd@users.noreply.github.com>
2025-09-22 17:04:57 -0400

    Integrate Acceptance tests
        Enable testing of the API at runtime
    
        # Co-authored-by: Oche "The Nigerian Nerd" Edache

---
+++ Pragmatic-Programmer/TDListAcceptanceTests/AcceptanceTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TDList.Classes;
+using Xunit;
+
+namespace TDListAcceptanceTests;
+
+public class AcceptanceTest
+{
+    [Fact]
+    public async Task ShouldReturnData()
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync("https://localhost:7101/api/todo");
+        Assert.Equal("application/json; charset=utf-8",
+            response.Content.Headers.ContentType?.ToString());
+    }
+    [Fact]
+    public async Task ShouldPrintToDoList()
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync("https://localhost:7101/api/todo");
+        var toDos = JsonConvert.DeserializeObject<List<ToDo>>(await response.Content.ReadAsStringAsync());
+        Assert.NotNull(toDos);
+    }
+}
